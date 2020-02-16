25301e2cfb2b76c656f869bded117b07f221004f
DmytroSmarasinghe <dima35x@ukr.net>
2020-02-16 11:36:01 +0200

    added new-items(all) and page

--- app/js/main.js
+++ app/js/main.js
@@ -10,9 +10,30 @@ $(function () {
 
     $(".product-slider__inner").slick({
         dots: true,
-        arrows:false,
-        slidesToShow:4,
-        slidesToScroll:4,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        // grid: true,
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function () {
+        $('.product-page__items>.product__item').addClass('list');
+        $('.icon-th-large').removeClass('active');
+        $('.icon-th-list').addClass('active');
+    });
+    $('.icon-th-large').on('click', function () {
+        $('.product-page__items>.product__item').removeClass('list');
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
     });
     var mixer = mixitup('.products__inner-box');
 });
