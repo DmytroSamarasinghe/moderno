49858249c99878c73293226e40b7a9aa2685af58
DmytroSmarasinghe <dima35x@ukr.net>
2020-02-24 12:10:55 +0200

    added settings

--- app/js/main.js
+++ app/js/main.js
@@ -32,10 +32,10 @@ $(function () {
         $('.icon-th-large').addClass('active');
     });
 
-    $('.product-one__tabs .tab').on('click', function(event) {
+    $('.product-one__tabs .tab, .settins__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $('.product-one__tabs, .settins__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs, .settins__tabs .tabs').find('.tab').removeClass('active');
             $(this).addClass('active');
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
